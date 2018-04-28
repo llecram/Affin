e7fa49386307d2b3062c3cc7acb0d5bb11dfcfe2
Dennis <31170537+llecram@users.noreply.github.com>
2018-04-28 00:04:30 -0500

    Add files via upload

---
+++ Cifrado Affin/Header.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_H_INCLUDED
+#define HEADER_H_INCLUDED
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <math.h>
+#include <stdio.h>
+using namespace std;
+int euclides(int, int);
+int euclidesext(int, int);
+int modulo(int, int);
+#endif // HEADER_H_INCLUDED
